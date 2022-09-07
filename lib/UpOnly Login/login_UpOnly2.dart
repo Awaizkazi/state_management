2b38db9caff9ef5e39972e45de6208582af8d459
Awaizkazi <88947135+Awaizkazi@users.noreply.github.com>
2022-09-07 19:03:35 +0530

    c

---
+++ lib/UpOnly Login/login_UpOnly2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginUpOnly2 extends StatefulWidget {
+  const LoginUpOnly2({super.key});
+
+  @override
+  State<LoginUpOnly2> createState() => _LoginUpOnly2State();
+}
+
+class _LoginUpOnly2State extends State<LoginUpOnly2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
